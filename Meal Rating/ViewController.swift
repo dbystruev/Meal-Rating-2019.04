ed5f401c1bd4d3bef1a1aebe14d7658ed79d4360
Denis Bystruev <dbystruev@me.com>
2019-04-08 20:39:20 +0300

    Initial Commit

---
+++ Meal Rating/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Meal Rating
+//
+//  Created by Denis Bystruev on 08/04/2019.
+//  Copyright © 2019 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
